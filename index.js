ef63ddce381abd6494f335be22b6d9471305e297
Zack <zackjob7498@gmail.com>
2021-04-13 14:50:18 +0800

    Test Github Action

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
+const { web } = require('./config')
 const app = new Koa();
-const port = 3000
+console.log(`Hello~~~~~~~~~~~~~~~Express~~~~~~~~~~~~~~~`);
 // response
+/*
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  ctx.body = web.message;
 });
-console.log('The server is listen at :', port);
-app.listen(port);
+console.log('The server is listen at :', web.port);
+app.listen(web.port);*/
