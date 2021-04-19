0a958736500a2a9790aded758b0b0bf070675042
Zack <zackjob7498@gmail.com>
2021-04-19 10:33:18 +0800

    add log

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const { web } = require('./config')
 const app = new Koa();
 console.log(`Hello~~~~~~~~~~~~~~~Express~~~~~~~~~~~~~~~`);
+console.log(`TEST ${process.env.TEST}`);
+console.log(`Hello ${process.env.HELLO}`);
 // response
 /*
 app.use(ctx => {
